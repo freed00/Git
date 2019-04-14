b251c7aa946d9b1b502194d0817f6ad46910b894
freed00 <mitch00@mail.ru>
2019-04-14 12:29:52 +0300

    Add subtraction
    Signed-off-by: freed00 <mitch00@mail.ru>

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main()
 	printf("Enter 2 num, please \n");
 	scanf("%d%d", &a, &b);
 	printf("\nSumm of a and b = %d\n", (a+b));
+	printf("\nSubtraction of a and b = %d\n", (a-b));
 
 	return 0;
 }
